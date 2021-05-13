dabcb8758a9c8244ecf7fe35e61e1a528d334a02
BlasAdayMedinaGonzalez <usr2702@salesianos-lacuesta.net>
2021-05-13 13:05:33 +0100

    Se ha arreglado el código testContadorExiste y pasa la prueba

---
+++ src/Contador.java
@@ -0,0 +1,12 @@
+
+
+public class Contador {
+    int valorInical = 0;
+    int valorlimite;
+    int valorIncremento = 1;
+    public Contador(int valorInical, int valorlimite, int valorIncremento) {
+        this.valorInical = valorInical;
+        this.valorlimite = valorlimite;
+        this.valorIncremento = valorIncremento;
+    }
+}
