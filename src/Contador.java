2dd54290bb66e24609ef8aeed3bc906891bd1559
BlasAdayMedinaGonzalez <usr2702@salesianos-lacuesta.net>
2021-05-14 01:48:41 +0100

    Realizando la prueba testContadorResetearValorActual

--- src/Contador.java
+++ src/Contador.java
@@ -59,4 +59,6 @@ public class Contador {
         }
         return this.valorInical;
     }
+
+    
 }
--- src/pruebaContador.java
+++ src/pruebaContador.java
@@ -36,6 +36,13 @@ public class pruebaContador {
         int expected = 8;
         assertEquals(expected, c.valorActual()); 
     }
+
+    @Test
+    public void testContadorResetearValorActual() {
+        Contador c = new Contador(0,2,6);
+        int expected = 0;
+        assertEquals(expected, c.resetear()); 
+    }
     
 
 }
